450a44362c414c8c256e73f69e6ac09078d4eba4
masudrana5 <abdullainfo30@gmail.com>
2023-04-11 20:22:57 +0600

    simple counterslice setup using rtk

--- rtk/features/counter/counterSlice.js
+++ rtk/features/counter/counterSlice.js
@@ -1,21 +1,20 @@
-const createSlice = require("@reduxjs/toolkit");
+const { createSlice } = require("@reduxjs/toolkit");
 
-// initial state
 const initialState = {
-    count: 0,
+  counter: 0,
 };
 
 const counterSlice = createSlice({
-    name: "counter",
-    initialState,
-    reducers: {
-        increment: (state, action) => {
-            state.count++;
-        },
-        decrement: (state, action) => {
-            state.count--;
-        },
+  name: "counter",
+  initialState,
+  reducers: {
+    increment: (state, action) => {
+      state.counter++;
     },
+    decrement: (state, action) => {
+      state.counter--;
+    },
+  },
 });
 
 modules.exports = counterSlice.reducer;
